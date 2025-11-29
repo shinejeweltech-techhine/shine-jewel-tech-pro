3af4347b06203de75cbc40cfbbb7128b0acef246
shinejeweltech-techhine <shinejeweltech@gmail.com>
2025-11-29 12:43:40 +0530

    Update data.ts

--- data.ts
+++ data.ts
@@ -8,7 +8,7 @@ export const machines: Machine[] = [
     description: 'High-precision laser cutting machine designed for intricate jewelry designs. Features minimal material loss technology.',
     priceRange: '$12,000 - $15,000',
     features: ['0.01mm Precision', 'Auto-Focus Head', 'Water Cooling System'],
-    imageUrl: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi-mKVPip0R8h8uf0dCsmd3cIUD_dJzvLxFBXT80fHWhD5PAAcW0zYCPMaVkO-xWDTZsEP2p_8OuP0_OHmyphRbmGb5l_zYa8Vz3w7nYZtWwoGbDxCqwvFJ6Fxss0hG_cSvYGgBAVFSo0FYZSk06FnIpQxnOsLwIkNXXgAE9UPrwtR6J2431-ubBiX3Rsg/s320/Gemini_Generated_Image_3kfjiu3kfjiu3kfj.png'
+    imageUrl: '/images/machine1.png'
   },
   {
     id: '2',
